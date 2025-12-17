3e6dfef168a902d16603a1b1014073eb1f2a4009
jasmyne222 <jasmine.degirmenci@heig-vd.ch>
2025-12-17 15:33:19 +0100

    add aggregation endpoints for activity statistics and user stats

--- humap-backend/src/routes/users.routes.js
+++ humap-backend/src/routes/users.routes.js
@@ -1,10 +1,12 @@
 import express from "express";
 import { getUser, updateUser, deleteUser } from "../controllers/user.controller.js";
+import { getUserStats } from "../controllers/activity.controller.js";
 import auth from "../middlewares/auth.js";
 
 const router = express.Router();
 
 router.get("/:id", getUser);
+router.get("/:userId/stats", getUserStats);
 router.patch("/:id", auth, updateUser);
 router.delete("/:id", auth, deleteUser);
 
